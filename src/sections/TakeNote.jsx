0c4997c3b1b1ff0a5c89bb77a969e43e1b21875e
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-04-30 03:15:28 +0530

    added CR functionality from firestore

--- src/sections/TakeNote.jsx
+++ src/sections/TakeNote.jsx
@@ -1,22 +1,56 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+//import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import CreateArea from "../components/CreateArea";
 import Note from "../components/Note";
 import Footer from "../components/Footer";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../utils/firebase";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  serverTimestamp,
+} from "firebase/firestore";
+
 export default function TakeNote() {
+  const [user,loading] = useAuthState(auth);
   const [notes, setNotes] = useState([]);
-  function addNote(newNote) {
-    setNotes((prevNotes) => {
-      return [...prevNotes, newNote];
+  const navigate = useNavigate();
+  
+  //reading data
+  const getData = async () => {
+    if (loading) return;
+    if (!user) return navigate("/");
+
+    const collectionRef = collection(db, `${user.uid}`);
+    const q = query(collectionRef, orderBy("timestamp","asc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setNotes(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     });
-  }
-  function deleteNote(id) {
-    setNotes((prevNotes) => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
+    return unsubscribe;
+  };
+
+  //write to database
+  const addNote = async(newNote) => {
+    const collectionRef = collection(db, `${user.uid}`);
+    console.log('test');
+      await addDoc(collectionRef, {
+        ...notes,
+        timestamp: serverTimestamp(),
+        title: newNote.title,
+        content: newNote.content,
+        username: user.displayName,
       });
-    });
   }
+
+  useEffect(() => {
+    getData();
+  }, [user, loading]);
+
   return (
     <div className="app min-h-screen relative flex flex-col gap-4 bg-zinc-100 dark:bg-zinc-900">
       <Header />
@@ -28,7 +62,7 @@ export default function TakeNote() {
             id={index}
             title={noteitem.title}
             content={noteitem.content}
-            onDelete={deleteNote}
+            //onDelete={deleteNote}
           />
         ))}
       </div>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth"
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +14,10 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_STORAGE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
