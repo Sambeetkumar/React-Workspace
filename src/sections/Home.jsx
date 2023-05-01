6477c7863a9993015ef47a45b267143552dd5458
sambeet kumar khatua <khatuasambeet31@gmail.com>
2023-05-02 03:38:36 +0530

    added github link to homepage

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,15 +1,11 @@
 import { React, useEffect } from "react";
-import {
-  signInWithPopup,
-  GoogleAuthProvider,
-} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../utils/firebase";
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
-
   const [user] = useAuthState(auth);
   const navigate = useNavigate();
   //Sign in with google
@@ -31,7 +27,15 @@ export default function Home() {
   }, [user]);
   return (
     <section className="bg-yellow-200 min-h-screen flex flex-col items-center justify-center">
-      <div className="px-6 py-8 lg:py-0 md:px-12 text-gray-800 text-center lg:text-left">
+      <div className="z-10 fixed top-0 w-full bg-gray-900 px-4 py-3 text-white">
+        <p className="text-center text-sm font-medium">
+          Love Note master?{" "}
+          <a href="https://github.com/Sambeetkumar/React-Workspace/tree/keeper-app" target="blank" className="inline-block underline underline-offset-4">
+            Give it a ⭐ on GitHub
+          </a>
+        </p>
+      </div>
+      <div className="px-6 py-8 lg:py-0 md:px-12 text-gray-800 text-center lg:text-left mt-8 md:mt-0 lg:mt-10">
         <div className="container mx-auto xl:px-32">
           <div className="grid lg:grid-cols-2 flex items-center">
             <div className="md:mt-12 lg:mt-0 mb-8 lg:mb-0">
@@ -49,8 +53,10 @@ export default function Home() {
                   Take notes anywhere! anytime!
                 </h2>
                 <p className="text-lg md:text-xl font-bold text-black mb-6">
-                  The NoteMaster is a free, open source and cloud based web app that allows user to take notes within
-                  the snap of a finger and also provides access to the notes from any device with an internet connection.
+                  The NoteMaster is a free, open source and cloud based web app
+                  that allows user to take notes within the snap of a finger and
+                  also provides access to the notes from any device with an
+                  internet connection.
                 </p>
                 <button
                   className="flex items-center justify-start mx-auto lg:ml-0 gap-2 w-56 md:w-64 inline-block p-3 mb-2 md:mb-0 bg-gray-800 text-white font-bold text-sm md:text-lg leading-snug rounded-lg shadow-md hover:bg-gray-950 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-950 active:shadow-lg transition duration-150 ease-in-out"
@@ -66,8 +72,8 @@ export default function Home() {
             <div className="md:mb-4 lg:mb-0">
               <img
                 src="https://images.unsplash.com/photo-1616628188467-8fb29f49bbe8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ2fHxub3Rlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                className="w-auto mx-auto lg:ml-0 rounded-lg shadow-lg max-h-[95vh]"
-                alt=""
+                className="w-auto mx-auto lg:ml-0 rounded-lg shadow-lg max-h-[90vh]"
+                alt="hero_img"
               />
             </div>
           </div>
